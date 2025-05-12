eed182c3b8c857181afc74e72dced8a9dfc41edf
Jorge Pacora <jpacora@outlook.com>
2025-05-11 22:42:30 -0500

    Updating deps

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ const tor = tor_axios.torSetup({
 const url = "http://api.ipify.org";
 const httpsUrl = "https://api.ipify.org";
 
-describe('토르 테스트', function(){
+describe('Tor test', function(){
 
     let realip = '';
     let torip = '';
@@ -23,51 +23,51 @@ describe('토르 테스트', function(){
         await tor.httpAgent();
     }).timeout(100000);
 
-    it('http로 아이피 확인', async function() {
+    it('Check IP with HTTP', async function() {
         let response = await axios.get(url);
         realip = response.data;
         console.log(realip);
         if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(realip)){
-            throw new Error('연결 실패');
+            throw new Error('Connection Error');
         }
 
     }).timeout(100000);
 
-    it('http로 토르 아이피 확인', async function() {
+    it('Check IP with HTTP over Tor', async function() {
         let torResponse = await tor.get(url);
         torip = torResponse.data;
         console.log(torip);
         if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(torip)){
-            throw new Error('연결 실패');
+            throw new Error('Connection Error');
         }
         
     }).timeout(10000);
 
-    it('https로 토르 아이피 확인', async function() {
+    it('Check IP with HTTPS over Tor', async function() {
         let torResponse = await tor.get(httpsUrl);
         torip = torResponse.data;
         //console.log(torip);
         if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(torip)){
-            throw new Error('연결 실패');
+            throw new Error('Connection Error');
         }
         
     }).timeout(10000);
 
-    it('realip와 tor아이피 확인', function() {
+    it('Check realip and tor ip', function() {
         if ( realip === torip ) {
-            throw new Error('토르 아이피와 지금 아이피가 같음');
+            throw new Error('Tor IP and current IP are the same');
         }
     })
 
-    it('토르 세션 새로운 세션 받기', async function() {
+    it('Tor Session Get a new session', async function() {
         await tor.torNewSession();
     });
 
-    it('토르 새로운세션 아이피 변화 확인', async function() {
+    it('Check Tor new session IP change', async function() {
         let after = await tor.get(url);
 
         if ( torip === after.data ) {
-            throw new Error('아이피 변경이 안됨');
+            throw new Error('IP address cannot be changed');
         }
 
         console.log(`before: ${torip}, after: ${after.data}`);
